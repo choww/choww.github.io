e44da362fc791d5fbc6a043722d2c5ae5a849330
Car <c_chow568@yahoo.ca>
2016-01-03 22:03:42 -0800

    Added jQuery effects

--- js/script.js
+++ js/script.js
@@ -1,8 +1,44 @@
 $(document).ready(function() {
+	/***** TYPE EFFECT *****/
+
+	setInterval(function() { blinkCursor() }, 1500);
+	function blinkCursor() {
+		$('.type-cursor').fadeTo(200, 0).fadeTo(100,1);
+	}
+
+	$('.backup-type').hide();	// in case jQ doesn't load
+
+	var chars = 'code'.split("");
+	var content = '';
+	$.each(chars, function(index, value) {
+		content += "<span style='display:none'>" + value + "</span>"
+	});
+	$('.type-effect').html(content);
+	$('.type-effect span').first().show("fast", function showNext() {
+		$(this).next("span").show(300, showNext);
+	});
+
+	/****** SLIDER *******/
 	$('.img-slider').slick({
 		fade: true,
 		infinite: true
-		
+	});
+
+	/***** STICKY TOP BUTTON ****/
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 200) {
+			$('.to-top').fadeIn(300);
+		}
+		else {
+			$('.to-top').fadeOut(300);
+		}
+	});
+
+	$('.to-top').click(function(e) {
+		e.preventDefault();
+		$('html, body').animate({scrollTop: 0}, 300);
+		return false;
 	});
 	
 });
