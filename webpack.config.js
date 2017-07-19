578bcef509b1f764dd8344aa2e879dbf6f50cd12
car <carmenn.choww@gmail.com>
2017-07-18 17:16:20 -0700

    set up react & webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + 'index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: __dirname + 'js/main.js',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    filename: 'main.js',
+    path: __dirname + '/build'
+  },
+  plugins: [HTMLWebpackPluginConfig]
+}
