a1ab9ef993d495586560f0dd760c6ffee265efa2
car <carmenn.choww@gmail.com>
2017-07-20 11:54:01 -0700

    fix webpack.config.js file names

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 var HTMLWebpackPlugin = require('html-webpack-plugin');
 
 var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-  template: __dirname + 'index.html',
+  template: __dirname + '/index.html',
   filename: 'index.html',
   inject: 'body'
 });
 
 module.exports = {
-  entry: __dirname + 'js/main.js',
+  entry: __dirname + '/js/main.js',
   module: {
     loaders: [
       {
